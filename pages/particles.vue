bfa6e4affb1cd66c8a5df7012d77faef210e2eda
Ruben <megarex333@gmail.com>
2019-03-07 19:05:38 +0100

    hero

--- pages/particles.vue
+++ pages/particles.vue
@@ -1,16 +1,33 @@
 <template>
-  <v-container fluid :class="{'px-0': $vuetify.breakpoint.smAndUp}">
-    <v-layout align-center>
+  <v-container fluid fill-height>
+    <v-layout align-center row wrap>
       <v-flex>
-        <particles />
-        <p>Hola</p>
+        <section class="hero">
+          <h1 
+            class="display-4 text-xs-center"
+          >
+            Hackers &lt;&gt; Week
+          </h1>
+          <!--<particles /> -->
+        </section> 
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
 <style>
+.hero {
+  height: 100%;
+}
+
+.hero-text {
+  position: absolute;
+  top: 40%;
+}
 </style>
+
 <script>
+/*
 import Particles from '~/components/Particles.vue'
 
 export default {
@@ -18,4 +35,5 @@ export default {
     Particles
   }
 }
+*/
 </script>

42c6e425f7704b547c43f85fbf06b64a765556f7
Guisanpea <guisanpea@gmail.com>
2019-03-07 20:31:48 +0100

    Particles component

--- components/Particles.vue
+++ components/Particles.vue
@@ -1,30 +1,134 @@
 <template>
-  <vue-particles
-    class="particles"
-    color="#dedede"
-    :particle-opacity="0.7"
-    :particles-number="30"
-    shape-type="circle"
-    :particle-size="4"
-    lines-color="#dedede"
-    :lines-width="1"
-    :line-linked="true"
-    :line-opacity="0.4"
-    :lines-distance="10"
-    :move-speed="3"
-    :hover-effect="true"
-    hover-mode="grab"
-    :click-effect="true"
-    click-mode="push"
-  />
+  <div id="particles" />
 </template>
 
 
 <style>
-.particles {
-  display: flex;
-  position: absolute;
-  flex-flow: column;
-  height: 100vh;
-}
 </style>
+
+<script>
+export default {
+  /*  global particlesJS */
+  mounted() {
+    this.$nextTick(() => {
+      require('particles.js')
+      this.initParticlesJS()
+    })
+  },
+
+  methods: {
+    initParticlesJS() {
+      particlesJS('particles', {
+        particles: {
+          number: {
+            value: 100,
+            density: {
+              enable: true,
+              value_area: 472.0061046122865
+            }
+          },
+          color: {
+            value: '#2196ba'
+          },
+          shape: {
+            type: 'circle',
+            stroke: {
+              width: 0,
+              color: '#000000'
+            },
+            polygon: {
+              nb_sides: 8
+            },
+            image: {
+              src: 'img/github.svg',
+              width: 100,
+              height: 100
+            }
+          },
+          opacity: {
+            value: 0.660808546457201,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 1,
+              opacity_min: 0.1,
+              sync: false
+            }
+          },
+          size: {
+            value: 2,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 40,
+              size_min: 0.1,
+              sync: false
+            }
+          },
+          line_linked: {
+            enable: true,
+            distance: 150,
+            color: '#1b677e',
+            opacity: 0.6765420832776106,
+            width: 0.9
+          },
+          move: {
+            enable: true,
+            speed: 5,
+            direction: 'none',
+            random: true,
+            straight: false,
+            out_mode: 'out',
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200
+            }
+          }
+        },
+        interactivity: {
+          detect_on: 'canvas',
+          events: {
+            onhover: {
+              enable: true,
+              mode: 'grab'
+            },
+            onclick: {
+              enable: true,
+              mode: 'remove'
+            },
+            resize: true
+          },
+          modes: {
+            grab: {
+              distance: 150,
+              line_linked: {
+                opacity: 1
+              }
+            },
+            bubble: {
+              distance: 400,
+              size: 40,
+              duration: 2,
+              opacity: 8,
+              speed: 3
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4
+            },
+            push: {
+              particles_nb: 4
+            },
+            remove: {
+              particles_nb: 2
+            }
+          }
+        },
+        retina_detect: true
+      })
+    }
+  }
+}
+</script>

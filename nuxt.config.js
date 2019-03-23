89ad3be99facc0fc47b80af16c379bc17585b080
Ruben <megarex333@gmail.com>
2019-03-23 14:35:01 +0100

    No zoom en movil

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,11 @@ module.exports = {
     title: 'Hackers <> Week',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+      },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [

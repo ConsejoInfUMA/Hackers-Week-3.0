dd5e4c2772d84a57ef3334fe00b9980e1fe880f9
Ruben <megarex333@gmail.com>
2019-04-07 22:36:50 +0200

    Ajustes

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
       {
         name: 'viewport',
         content:
-          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui'
+          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, content="minimal-ui"'
       },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
